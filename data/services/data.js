bd8fa2f48af192a4b3d44514bd7d2ea3ed20bf73
MSUResult <shivanshsingh4539@gmail.com>
2026-05-03 07:42:51 +0530

    checking

--- data/services/data.js
+++ data/services/data.js
@@ -1,13 +1,13 @@
-export const services =[
-    {
-        slug:"ecommerce-website-development-cost-india",
-        seo:{
-            "",
-            "",
+// export const services =[
+//     {
+//         slug:"ecommerce-website-development-cost-india",
+//         seo:{
+//             "",
+//             "",
 
         
-        },
+//         },
 
 
-    }
-]
+//     }
+// ]
